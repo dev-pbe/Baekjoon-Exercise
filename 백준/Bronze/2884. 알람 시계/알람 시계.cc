ddc6f3636a146ae28964b96da8a6375859928da6
dev-pbe <104449890+dev-pbe@users.noreply.github.com>
2022-04-30 15:53:57 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	int H = 0;
+	int M = 0;
+	
+	scanf("%d %d", &H, &M);
+	
+	if (H == 0){
+		if (M < 45){
+			H = 23;
+			M = (M + 60) - 45;
+			printf("%d %d", H, M);
+		}
+		else if (M >= 45){
+			M = M - 45;
+			printf("%d %d", H, M);
+		}
+	}
+	
+	else if (H > 0){
+		if (M < 45){
+			H = H - 1;
+			M = (M + 60) - 45;
+			printf("%d %d", H, M);
+		}
+		else if (M >= 45){
+			M = M - 45;
+			printf("%d %d", H, M);
+		}
+	}
+}
